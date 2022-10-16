d0abece550aa2a27634c74187630bbb32a752a16
Roman <zh.rm@internet.ru>
2022-10-16 19:18:04 +0300

    Comment to Random

--- Lesson_1.Random/Program.cs
+++ Lesson_1.Random/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Сложение двух чисел. Первое случайное от 1 до 10, второе - заданное пользователем. Введите любое целое число: ");
-int a = new Random().Next(1, 11);
+int a = new Random().Next(1, 11); // 1, 2, 3, ... 10
 int b = int.Parse(Console.ReadLine());
 int c = a + b;
 Console.Write("Сумма случайного числа " + a);
