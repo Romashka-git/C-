565ece1126433c6d97d821d9858774382d3921b0
Roman <zh.rm@internet.ru>
2022-10-16 18:48:05 +0300

    Суммирование введенных чисел (int.Parse)

--- Lesson_1.Sum/Program.cs
+++ Lesson_1.Sum/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сумма двух чисел
+
+
+Console.WriteLine("Введите два целых числа для суммирования: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = a + b;
+Console.Write("Сумма чисел равна ");
+Console.WriteLine(c);
