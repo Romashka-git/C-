f601fb5d157ace68015b598f4e846b50c2de7c80
Roman <zh.rm@internet.ru>
2022-10-19 00:15:50 +0300

    Приветские Маши

---
+++ Lesson_1.Masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же " + username);
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
