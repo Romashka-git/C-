c2001e43792e652624d23106a64b0fe1d22bac14
Roman <zh.rm@internet.ru>
2022-10-19 21:44:54 +0300

    Задача на нахождение наибольшего числа

---
+++ Lesson_1.Weight/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача найти максимально тяжелую гирю.
+
+
+int a = 6;
+int b = 5;
+int c = 8;
+int d = 1;
+int e = 9;
+int max = a; 
+
+if (b > max) max = b;
+if (a > max) max = a;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
