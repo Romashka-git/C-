ae31e33dca59fc31aa7ff3ec454baf4a176621f3
Roman <zh.rm@internet.ru>
2022-10-22 00:37:31 +0300

    Модифицировали код в программе квадрата

--- Lesson_1/Seminar_1.x_2/Program.cs
+++ Lesson_1/Seminar_1.x_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Напистаь программу, принимающая на вход число и выдающая квадрат этого числа
 
 Console.Write("Введите чило для возведение его в квадрат: ");
-int a = int.Parse(Console.ReadLine());
+// int a = int.Parse(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 int b = a * a;
 Console.Write("Квадрат числа " + a + " равен " + b);
