2eff0559b5cb921f220da426353d42beaa7d287e
Roman <zh.rm@internet.ru>
2022-11-05 19:30:52 +0300

    Добавили программу для вычисления дня недели

---
+++ Lesson_1/Seminar_1.Days/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу, которая будет выдавать название
+// дня недели по заданному номеру.
+
+
+string monday =  "Понедельник";
+string tuesday =  "Вторник";
+string wednesday =  "Среда";
+string thursday =  "Четверг";
+string friday =  "Пятница";
+string saturday =  "Суббота";
+string sunday =  "Воскресенье";
+
+Console.Write("Введите номер дня, а я напишу, какой это день недели. ");
+int answer = int.Parse(Console.ReadLine());
+if (answer == 1)
+{
+    Console.WriteLine(monday);
+}
+if (answer == 2)
+{
+    Console.WriteLine(tuesday);
+}
+if (answer == 3)
+{
+    Console.WriteLine(wednesday);
+}
+if (answer == 4)
+{
+    Console.WriteLine(thursday);
+}
+if (answer == 5)
+{
+    Console.WriteLine(friday);
+}
+if (answer == 6)
+{
+    Console.WriteLine(saturday);
+}
+if (answer == 7)
+{
+    Console.WriteLine(sunday);
+}
