ef23ea685d745c573bc2f6507e5a3087efc73f01
Roman <zh.rm@internet.ru>
2022-11-05 14:26:31 +0300

    Добавили программу на проверку квадрата второго числа

---
+++ Lesson_1/Lesson_1.Square_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает 2 числа
+// и проверяет, является ли первое число квадратом второго
+
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+{
+    System.Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("NO!");
+}
