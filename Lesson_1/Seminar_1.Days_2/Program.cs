36c4dfa37e723614233219ebe6958e3fccdca534
Roman <zh.rm@internet.ru>
2022-11-08 17:48:58 +0300

    Добавили алгоритм решения задачи со днями недели с помощью массива

---
+++ Lesson_1/Seminar_1.Days_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая будет выдавать название
+// дня недели по заданному номеру.
+
+System.Console.WriteLine("Введите числоЮ соответствующее дню недели: ");
+int answer = int.Parse(Console.ReadLine());
+
+string[] days = new string[7] { "пн", "вт", "ср", "чт", "пт", "сб", "вс", };
+
+if (answer > 0 && answer < 8)
+{
+    Console.WriteLine(days[answer - 1]);
+}
+else
+{
+    Console.WriteLine("Необходимо ввести число от 1 до 7");
+}
