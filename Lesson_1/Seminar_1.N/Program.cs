555cf97c38279830379270a2eb3aa545b5673594
Roman <zh.rm@internet.ru>
2022-11-10 06:23:37 +0300

    Добавили программу по выводу от -N до N

---
+++ Lesson_1/Seminar_1.N/Program.cs
@@ -0,0 +1,14 @@
+﻿// написать программу, которая на вход принимает одно число,
+// а на выходе показывает все целые числа от -N до N
+
+
+System.Console.Write("Введите целое положительное число: ");
+int n = int.Parse(Console.ReadLine());
+
+int count = -n;
+
+while (count < n + 1)
+{
+    Console.Write(count + " ");
+    count++;
+}
