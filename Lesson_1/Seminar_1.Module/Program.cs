53983e0fde4f97830a663995ed346ef32fc994ae
Roman <zh.rm@internet.ru>
2022-11-10 06:47:19 +0300

    Добавили программу по вычислению модуля числа

---
+++ Lesson_1/Seminar_1.Module/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу вычисления модуля числа
+
+
+System.Console.WriteLine("Введите число, а я выведу его модуль: ");
+int a = int.Parse(Console.ReadLine());
+int var = a;
+
+if (a > 0)
+{
+    Console.WriteLine("Модуль числа " + var + " равен: " + a);
+} 
+if (a < 0)
+{
+    a = a * -1;
+    Console.WriteLine("Модуль числа " + var + " равен: " + a);
+}
